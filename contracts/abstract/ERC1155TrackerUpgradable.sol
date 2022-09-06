f5d737400cfcddbe41ed5bc77f84082109cff8cf
Roy <toledoroy@gmail.com>
2022-09-06 13:02:06 -0700

    Cleanup - Removed _uri variable and functions

--- contracts/abstract/ERC1155TrackerUpgradable.sol
+++ contracts/abstract/ERC1155TrackerUpgradable.sol
@@ -35,9 +35,6 @@ abstract contract ERC1155TrackerUpgradable is
     // Mapping from account to operator approvals
     mapping(address => mapping(address => bool)) private _operatorApprovals;
 
-    // Used as the URI for all token types by relying on ID substitution, e.g. https://token-cdn-domain/{id}.json
-    // string private _uri;
-
     // Manage Balances by External Token ID
     mapping(uint256 => mapping(uint256 => uint256)) private _balances;
 
@@ -79,20 +76,6 @@ abstract contract ERC1155TrackerUpgradable is
             interfaceId == type(IERC1155Upgradeable).interfaceId ||
             super.supportsInterface(interfaceId);
     }
-
-    /**
-     * @dev See {IERC1155MetadataURI-uri}.
-     *
-     * This implementation returns the same URI for *all* token types. It relies
-     * on the token type ID substitution mechanism
-     * https://eips.ethereum.org/EIPS/eip-1155#metadata[defined in the EIP].
-     *
-     * Clients calling this function must replace the `\{id\}` substring with the
-     * actual token type ID.
-     */
-    // function uri(uint256) public view virtual override returns (string memory) {
-    //     return _uri;
-    // }
 
     /**
      * @dev See {IERC1155-balanceOf}.
@@ -292,29 +275,6 @@ abstract contract ERC1155TrackerUpgradable is
         
         _doSafeBatchTransferAcceptanceCheck(operator, from, to, ids, amounts, data);
     }
-
-    /**
-     * @dev Sets a new URI for all token types, by relying on the token type ID
-     * substitution mechanism
-     * https://eips.ethereum.org/EIPS/eip-1155#metadata[defined in the EIP].
-     *
-     * By this mechanism, any occurrence of the `\{id\}` substring in either the
-     * URI or any of the amounts in the JSON file at said URI will be replaced by
-     * clients with the token type ID.
-     *
-     * For example, the `https://token-cdn-domain/\{id\}.json` URI would be
-     * interpreted by clients as
-     * `https://token-cdn-domain/000000000000000000000000000000000000000000000000000000000004cce0.json`
-     * for token type ID 0x4cce0.
-     *
-     * See {uri}.
-     *
-     * Because these URIs cannot be meaningfully represented by the {URI} event,
-     * this function emits no events.
-     */
-    // function _setURI(string memory newuri) internal virtual {
-    //     _uri = newuri;
-    // }
 
     /// Mint for Address Owner
     function _mint(address to, uint256 id, uint256 amount, bytes memory data) internal virtual {
